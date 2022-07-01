fac9eff81dca3cbb9368e4dcda1432c3d22987ad
hyunjoo03199 <108320614+hyunjoo03199@users.noreply.github.com>
2022-07-01 10:54:23 +0900

    Add files via upload

--- script/popup.js
+++ script/popup.js
@@ -6,3 +6,11 @@ $(function(){
         $('.popup').hide();
     });
 });
+$(function(){
+    $('.popup2').click(function(){
+        $('.popup').fadeIn();
+    });
+    $('.popup a').click(function(){
+        $('.popup').hide();
+    });
+});
